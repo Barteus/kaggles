09428c49d46ecea337e72fc84950b2b1bb574046
Ubuntu <ubuntu@ip-172-31-6-78.eu-west-1.compute.internal>
2019-01-29 15:12:30 +0000

    dog and house changes

--- houses/get-data.sh
+++ houses/get-data.sh
@@ -3,10 +3,11 @@
 pip install kaggle
 rm -R data
 mkdir data
-kaggle competitions download -c dog-breed-identification -p data
+kaggle competitions download -c house-prices-advanced-regression-techniques -p data
 
-unzip -q data/train -d data
-unzip -q data/test -d data
-unzip -q data/labels.csv.zip -d data
-unzip -q data/sample_submission.csv.zip -d data
-rm data/*.zip
+#Not needed for this competition dataset
+#unzip -q data/train -d data
+#unzip -q data/test -d data
+#unzip -q data/labels.csv.zip -d data
+#unzip -q data/sample_submission.csv.zip -d data
+#rm data/*.zip
